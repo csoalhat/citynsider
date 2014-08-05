6c143d5622c8054a48e167ddd46f893410a7342d
csoalhat <csoalhat@gmail.com>
2014-08-05 12:52:57 +0100

    added comments in js

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -2,11 +2,12 @@ function initialise(data) {
 
   if ((typeof data == 'undefined') && (typeof businesses_info == 'undefined')) {
     return false
-  //
+  // avoid errors
   }
 
   data = data || businesses_info
 
+  //map options 
   var mapOptions = {
     center: new google.maps.LatLng(51.5, -0.1),
     zoom: 15,
@@ -14,9 +15,13 @@ function initialise(data) {
     styles: [{'featureType':'water','stylers':[{'visibility':'on'},{'color':'#acbcc9'}]},{'featureType':'landscape','stylers':[{'color':'#f2e5d4'}]},{'featureType':'road.highway','elementType':'geometry','stylers':[{'color':'#c5c6c6'}]},{'featureType':'road.arterial','elementType':'geometry','stylers':[{'color':'#e4d7c6'}]},{'featureType':'road.local','elementType':'geometry','stylers':[{'color':'#fbfaf7'}]},{'featureType':'poi.park','elementType':'geometry','stylers':[{'color':'#c5dac6'}]},{'featureType':'administrative','stylers':[{'visibility':'on'},{'lightness':33}]},{'featureType':'road'},{'featureType':'poi.park','elementType':'labels','stylers':[{'visibility':'on'},{'lightness':20}]},{},{'featureType':'road','stylers':[{'lightness':20}]}]
   };
 
+  //create map
   var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
+  //run addAllpins
   addAllPins(map, data);
+
+  // define waypoints for directions
   var firstDatum = data.shift();
   var start = firstDatum.address.lat + "," + firstDatum.address.lng;
   var end = start;
@@ -33,6 +38,7 @@ function updateMap(location, keyword) {
   console.log('This is your keyword: ' + keyword);
   console.log('This is your location: ' + location);
 
+  //ajax call getting results returned from yelp
   $.ajax({
     data: { keyword: keyword, location: location}, 
     url: '/maps.json',
@@ -52,6 +58,7 @@ function setupEventHandlers() {
 
 
 function addMarker(map, position, name, display_address) {
+  //marker icon options
   var icon = new google.maps.MarkerImage(
     "http://i.imgur.com/N2ZEY5i.png", //url
     new google.maps.Size(32, 32), //size
@@ -64,6 +71,7 @@ function addMarker(map, position, name, display_address) {
     icon: icon
   });
 
+  // infoWindow information 
   infoWindow = new google.maps.InfoWindow();
     google.maps.event.addListener(marker, 'click', function() {
       if(infoWindow != undefined) infoWindow.close();
@@ -76,6 +84,7 @@ function addMarker(map, position, name, display_address) {
 
 }
 
+// getting all the details for each business returned by yelp
 function addAllPins(map, businesses_info) {
   for (i=0; i<businesses_info.length; i++){ 
     addMarker(map, businesses_info[i].address, businesses_info[i].name, businesses_info[i].display_address);
@@ -84,6 +93,7 @@ function addAllPins(map, businesses_info) {
   }
 }
 
+// directions 
 function calcRoute(map, start, end, data) {
   var directionsDisplay;
   var directionsService = new google.maps.DirectionsService();
